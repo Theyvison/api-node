4e589c07bbfbf5c6b4504fba00fd4c2c0dd73baf
Theyvison <theyvison.jf@hotmail.com>
2025-03-09 18:08:59 -0300

    first commit

---
+++ src/supabaseConnection.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_KEY || "";
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false
+  }
+})
